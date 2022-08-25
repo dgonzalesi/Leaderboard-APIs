d068017fb6cee44d828180a4b000f3bed21573d2
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-08-24 22:17:54 -0500

    Remove consLog and unused code in mainClass.js

--- src/index.js
+++ src/index.js
@@ -3,54 +3,6 @@ import Leaderboard from './modules/mainClass.js';
 console.log('Hello World from Webpack');
 // POST:
 
-// const game = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/0j4WGZsTGaiHYHiDDMYw/scores/';
-// const myfunctionSCORE = (url = '', data = {}) => {
-//   const option = {
-//     method: 'POST',
-//     mode: 'cors',
-//     headers: { 'Content-type': 'application/json; charset=UTF-8' },
-//     body: JSON.stringify(data),
-//   };
-
-//   fetch(url, option)
-//     .then((response) => response.json()).then((data) => console.log(data.result));
-// };
-
-// const dataSCORE = {
-//   user: 'Juan Perez',
-//   score: '99',
-// };
-
-// myfunctionSCORE(game, dataSCORE);
-// myfunctionSCORE(game, dataSCORE);
-// myfunctionSCORE(game, dataSCORE);
-// myfunctionSCORE(game, dataSCORE);
-// myfunctionSCORE(game, dataSCORE);
-// myfunctionSCORE(game, dataSCORE);
-
-// const myfunctionGET = (url = '') => {
-//   fetch(url)
-//     .then((response) => response.json())
-//     .then((data) => console.log(data.result))
-//     .catch(() => console.log('fatality error'));
-// };
-
-// myfunctionGET(game);
-
-// const mytest = fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/').then((res) => { console.log(res.json()); }).catch((err) => { console.log(err.ok); });
-
-// POST: https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/
-// GET: https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/0j4WGZsTGaiHYHiDDMYw/scores/
-// TEMPOGAME: 0j4WGZsTGaiHYHiDDMYw
-
 const leaderboard = new Leaderboard();
-
-// leaderboard.newPost('Juan Perez 1', '100');
-// leaderboard.newPost('Juan Perez 2', '200');
-// leaderboard.newPost('Juan Perez 3', '300');
-// leaderboard.newPost('Juan Perez 4', '400');
-// leaderboard.newPost('Juan Perez 5', '500');
-// leaderboard.newPost('Juan Perez 6', '600');
-// leaderboard.newPost('Juan Perez 7', '700');
 
 console.log(leaderboard.getScores());
--- src/modules/mainClass.js
+++ src/modules/mainClass.js
@@ -1,12 +1,11 @@
 export default class Leaderboard {
   constructor() {
-    // this.name = name;
     this.gameName = 'My New Game';
-    // this.score = score;
     this.currentGame = 'iqwJZVwWQ0yUJPTVp5Tk';
     this.url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
   }
 
+  // GET A NEW GAME KEY
   newGame = () => {
     const option = {
       method: 'POST',
@@ -22,6 +21,8 @@ export default class Leaderboard {
       .catch(() => new Error('Error in newPost'));
   }
 
+
+  //  POST THE DATA TO THE API
   newPost = (name, getscore) => {
     const option = {
       method: 'POST',
@@ -30,11 +31,10 @@ export default class Leaderboard {
       body: JSON.stringify({ user: name, score: getscore }),
     };
     fetch(`${this.url + this.currentGame}/scores/`, option)
-      .then((response) => response.json())
-      .then((data) => console.log(data.result))
       .catch(() => new Error('Error in newPost'));
   }
 
+  // GET THE DATA FROM THE API
   getScores = () => {
     this.scores = [];
     fetch(`${this.url + this.currentGame}/scores/`)
